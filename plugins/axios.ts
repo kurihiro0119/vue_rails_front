d5c3b0be08f55ee34da4c0452ce1e6448b073547
kurihiro <kuriharahironari@gmail.com>
2021-11-29 23:52:02 +0900

    Add axios

---
+++ plugins/axios.ts
@@ -0,0 +1,13 @@
+export default ({ $axios }: any) => {
+    $axios.onRequest((config : any) => {
+        console.log(config)
+    })
+
+    $axios.onResponse((config: any) => {
+        console.log(config)
+    })
+
+    $axios.onError((e: any) => {
+        console.log(e.response)
+    })
+}
