a874fdfe7be8cc901a4ee8e0f7072d9b3966c82c
root <root@DESKTOP-5SC5F3K.localdomain>
2022-02-08 02:00:29 +0900

    add navigation

---
+++ layouts/project.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+        <logged-in-app-bar
+          clipped-left
+        >
+          <template #navigation-toggle-button
+          >
+            <v-app-bar-nav-icon 
+              @click="drawer = !drawer"
+            />
+          </template>
+        </logged-in-app-bar>
+        <!-- navigation drawer -->
+        <project-navigation-drawer 
+          :drawer.sync="drawer"
+        />
+        <v-main>
+          project.vue
+          <nuxt />
+        </v-main>
+  </v-app>
+</template>
+
+<script>
+export default{
+  data() {
+    return {
+      drawer: null
+    }
+  },
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
